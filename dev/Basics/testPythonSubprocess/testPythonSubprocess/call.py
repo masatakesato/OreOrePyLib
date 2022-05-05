245ccc38959dfd8f05335a4ad79fcb39c5294be0
masatake1115 <masatake1115@hotmail.co.jp>
2022-05-05 11:55:00 +0900

    moved resources directory. created utils

--- dev/Basics/testPythonSubprocess/testPythonSubprocess/call.py
+++ dev/Basics/testPythonSubprocess/testPythonSubprocess/call.py
@@ -1,36 +1,8 @@
-﻿import os
+﻿from utils import environment
+environment.AddPythonEnvironmentPaths( None )#pathlib.Path( sys.executable ).parents[0] )
+
+import os
 import sys
-import pathlib
-
-
-
-envpath = pathlib.Path( sys.executable ).parents[0]
-
-#print( str(envpath).replace("\\", "\\\\") )
-
-g_EnvSubpaths =(
-    # Library
-    "/Library/mingw-w64/bin;",
-    "/Library/usr/bin;",
-    "/Library/bin;",
-    # Scripts
-    "/Scripts;",
-    # bin
-    "/bin;",
-    )
-
-rootpath = str(envpath).replace("\\", "/")
-for subpath in g_EnvSubpaths:
-    print( rootpath + subpath )
-
-    #sys.path.append( rootpath + subpath )# こっちじゃない
-    os.environ["PATH"] += rootpath + subpath
-
-
-print( os.environ["PATH"] )
-
-
-
 
 
 from time import sleep
@@ -39,11 +11,12 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
 
-print( sys.version )
-print( qVersion() )
 
+if __name__=="__main__":
 
+    print( sys.version )
+    print( qVersion() )
 
-sleep(1)  #一秒寝てもらう(非同期になっているか確認のため)
-args = sys.argv[1]
-print( "argv[1]",  args )
+    sleep(1)  #一秒寝てもらう(非同期になっているか確認のため)
+    args = sys.argv[1]
+    print( "argv[1]",  args )
---
+++ oreorepylib/utils/environment.py
@@ -0,0 +1,25 @@
+﻿import sys
+import os
+import pathlib
+
+
+
+def AddPythonEnvironmentPaths( executable=None ):
+
+    subDirectories =(
+        # Library
+        "Library/mingw-w64/bin",
+        "Library/usr/bin",
+        "Library/bin",
+        # Scripts
+        "Scripts",
+        # bin
+        "bin",
+        )
+
+    envroot = pathlib.Path( executable if executable else sys.executable ).parents[0]#pathlib.Path( sys.executable ).parents[0]
+
+    for subdir in subDirectories:
+        subpath = pathlib.Path.joinpath( envroot, subdir )
+        #print( subpath )
+        os.environ["PATH"] += str(subpath) + ";"
