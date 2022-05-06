2f5604f412ea83fc581c3418622a700848a8cbf6
masatake1115 <masatake1115@hotmail.co.jp>
2022-05-06 10:43:04 +0900

    Updated serializer.py  -umsgpack support for dcc tools.

---
+++ oreorepylib/network/serializerimpl_msgpack.py
@@ -0,0 +1,41 @@
+﻿import msgpack
+import traceback
+
+
+#class PackError(Exception): pass
+#class UnpackError(Exception): pass
+
+
+
+class Serializer:
+
+    def __init__( self, pack_encoding, unpack_encoding ):
+        print( "Serializer msgpack ver..." )
+        ##self.__m_Encode = encode
+        self.__m_Pakcer = msgpack.Packer(use_bin_type=True)# encoding=pack_encoding )
+        self.__m_Unpackcer = msgpack.Unpacker(raw=False)# encoding=unpack_encoding )
+
+
+
+    def Pack( self, data ):
+        try:
+            return self.__m_Pakcer.pack( data )
+
+        except:
+            print( 'Exception occured at Serializer::Pack ')
+            traceback.print_exc()
+            #raise PackError( traceback.format_exc() )
+            return None
+
+
+    def Unpack( self, data ):
+        try:
+            self.__m_Unpackcer.feed( data[:len(data)] )
+            for o in self.__m_Unpackcer:
+                return o
+
+        except:
+            print( 'Exception occured at Serializer::Unpack ')
+            traceback.print_exc()
+            #raise UnpackError( traceback.format_exc() )
+            return None
---
+++ oreorepylib/network/serializerimpl_umgspack.py
@@ -0,0 +1,37 @@
+﻿import umsgpack
+import traceback
+
+
+#class PackError(Exception): pass
+#class UnpackError(Exception): pass
+
+
+
+class Serializer:
+
+    def __init__( self, pack_encoding, unpack_encoding ):
+        print( "Serializer umsgpack ver..." )
+
+
+
+    def Pack( self, data ):
+        try:
+            return umsgpack.packb( data )
+
+        except:
+            print( 'Exception occured at Serializer::Pack ')
+            traceback.print_exc()
+            #raise PackError( traceback.format_exc() )
+            return None
+
+
+
+    def Unpack( self, data ):
+        try:
+            return umsgpack.unpackb( data[:len(data)] )
+
+        except:
+            print( 'Exception occured at Serializer::Unpack ')
+            traceback.print_exc()
+            #raise UnpackError( traceback.format_exc() )
+            return None
