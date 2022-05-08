f30412edbe816696e2cd183f6209023deedbd2a7
masatake1115 <masatake1115@hotmail.co.jp>
2022-05-08 10:18:51 +0900

    moved python codes from hogehogeproject.

---
+++ dev/Basics/testReorderList/testReorderList/testReorderList.py
@@ -0,0 +1,14 @@
+﻿mylist  = [ 'a','b','c','d','e' ]
+myorder = [  3,  2,  0,  1,  4  ]
+
+print( "original list..." )
+print( mylist, "\n" )
+
+print( "specified order..." )
+print( myorder, "\n\n" )
+
+
+print( "reordered list..." )
+resultlist = [ x for _,x in sorted(zip(myorder, mylist)) ]
+print( resultlist, "\n" )
+
