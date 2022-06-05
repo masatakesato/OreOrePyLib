bcf923496866219e94d4fd1fecc63e6f06b4b57c
masatake1115 <masatake1115@hotmail.co.jp>
2022-06-05 14:28:13 +0900

    added logger.py

---
+++ dev/Basics/testLogger/testLogger/testLogger_main.py
@@ -0,0 +1,18 @@
+﻿import traceback
+from oreorepylib.logger.logger import Logger
+
+
+
+if __name__ == "__main__":
+
+    logger = Logger(__name__)
+    logger.info('info_')
+
+    try:
+       cmds.polySphere()   
+       a = 10 / 0
+
+    except Exception as e:
+       logger.critical( traceback.format_exc() )
+
+    logger.close()
---
+++ oreorepylib/logger/logger.py
@@ -0,0 +1,45 @@
+﻿from logging import Formatter, handlers, StreamHandler, getLogger, DEBUG
+
+
+class Logger:
+
+    def __init__( self, name=__name__, filepath='D:/oreorelog.txt' ):
+        self.logger = getLogger(name)
+        self.logger.propagate = False
+        self.logger.setLevel(DEBUG)
+
+        if( not len(self.logger.handlers) ):
+            formatter = Formatter( '[%(asctime)s] [%(process)d] [%(name)s] [%(levelname)s]\n %(message)s\n' )
+
+            # stdout
+            handler = StreamHandler()
+            handler.setLevel(DEBUG)
+            handler.setFormatter(formatter)
+            self.logger.addHandler(handler)
+
+            # file
+            handler = handlers.RotatingFileHandler( filename = filepath, maxBytes = 1048576, backupCount = 3 )
+            handler.setLevel(DEBUG)
+            handler.setFormatter(formatter)
+            self.logger.addHandler(handler)
+        
+
+    def close( self ):
+        for handler in self.logger.handlers:
+            handler.close()
+        
+
+    def debug( self, msg ):
+        self.logger.debug(msg)
+
+
+    def info( self, msg ):
+        self.logger.info(msg)
+
+
+    def warn( self, msg ):
+        self.logger.error(msg)
+
+
+    def critical( self, msg ):
+        self.logger.critical(msg)
