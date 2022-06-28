9e822e16e12bf745851b29227498b69e4ef72d01
masatake1115 <masatake1115@hotmail.co.jp>
2022-06-28 16:33:14 +0900

    Refactoring GraphicsItemLayer

--- dev/PyQt/testGraphicsItemLayout/testGraphicsItemLayer/graphicsitemlayer.py
+++ dev/PyQt/testGraphicsItemLayout/testGraphicsItemLayer/graphicsitemlayer.py
@@ -1,9 +1,7 @@
-﻿import sys
-import time
-
-from PyQt5.QtCore import *
+﻿from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+
 
 
 class LayerRoot( QGraphicsItem ):
@@ -30,9 +28,7 @@ class LayerRoot( QGraphicsItem ):
         item.setParentItem( None )
 
 
-
-
-######################### QGraphicsItem func override ################################
+    ######################### QGraphicsItem func override ################################
 
     def boundingRect( self ):
         return self.childrenBoundingRect()
@@ -204,56 +200,3 @@ class GraphicsItemLayer:
 
 
 
-
-
-
-if __name__ == "__main__":
-
-    app = QApplication( sys.argv )
-
-    scene = QGraphicsScene()
-    view = QGraphicsView()
-    view.setEnabled( True )
-    view.setScene( scene )
-
-    view.setGeometry( 0, 0, 800, 600 )
-    view.show()
-
-
-    layer = GraphicsItemLayer( scene )
-    layer.AddLayer()
-    layer.AddLayer()
-
-
-    rect = QGraphicsRectItem()
-    rect.setRect( 0, 0, 60, 20 )
-    rect.setBrush( Qt.red )
-    rect.setFlag( QGraphicsItem.ItemSendsGeometryChanges )
-    rect.setFlag( QGraphicsItem.ItemIsMovable )
-    rect.setFlag( QGraphicsItem.ItemIsSelectable )
-    #rect.setFlag( QGraphicsItem.ItemIsFocusable, False )
-
-    layer.AddItem( rect, 0 )
-
-    rect2 = QGraphicsRectItem()
-    rect2.setRect( 10, 10, 60, 20 )
-    rect2.setBrush( Qt.green )
-    rect2.setFlag( QGraphicsItem.ItemSendsGeometryChanges )
-    rect2.setFlag( QGraphicsItem.ItemIsMovable )
-    rect2.setFlag( QGraphicsItem.ItemIsSelectable )
-    #rect2.setFlag( QGraphicsItem.ItemIsFocusable, False )
-
-    layer.AddItem( rect2, 1 )
-
-
-    layer.DeleteItem( rect, 0 )
-    del rect
-    #layer.DeleteItem( rect2, 1 )
-    #del rect2
-
-    #layer.MoveLayer(0, 1)
-
-
-
-
-    sys.exit( app.exec_() )
---
+++ dev/PyQt/testGraphicsItemLayout/testGraphicsItemLayer/testGraphicsItemLayer_main.py
@@ -0,0 +1,59 @@
+﻿import sys
+
+
+from graphicsitemlayer import *
+
+
+
+
+
+if __name__ == "__main__":
+
+    app = QApplication( sys.argv )
+
+    scene = QGraphicsScene()
+    view = QGraphicsView()
+    view.setEnabled( True )
+    view.setScene( scene )
+
+    view.setGeometry( 0, 0, 800, 600 )
+    view.show()
+
+
+    layer = GraphicsItemLayer( scene )
+    layer.AddLayer()
+    layer.AddLayer()
+
+
+    rect = QGraphicsRectItem()
+    rect.setRect( 0, 0, 60, 20 )
+    rect.setBrush( Qt.red )
+    rect.setFlag( QGraphicsItem.ItemSendsGeometryChanges )
+    rect.setFlag( QGraphicsItem.ItemIsMovable )
+    rect.setFlag( QGraphicsItem.ItemIsSelectable )
+    #rect.setFlag( QGraphicsItem.ItemIsFocusable, False )
+
+    layer.AddItem( rect, 0 )
+
+    rect2 = QGraphicsRectItem()
+    rect2.setRect( 10, 10, 60, 20 )
+    rect2.setBrush( Qt.green )
+    rect2.setFlag( QGraphicsItem.ItemSendsGeometryChanges )
+    rect2.setFlag( QGraphicsItem.ItemIsMovable )
+    rect2.setFlag( QGraphicsItem.ItemIsSelectable )
+    #rect2.setFlag( QGraphicsItem.ItemIsFocusable, False )
+
+    layer.AddItem( rect2, 1 )
+
+
+    #layer.DeleteItem( rect, 0 )
+    #del rect
+    #layer.DeleteItem( rect2, 1 )
+    #del rect2
+
+    layer.MoveLayer(0, 1)
+    layer.MoveLayer(1, 0)
+
+
+
+    sys.exit( app.exec_() )
