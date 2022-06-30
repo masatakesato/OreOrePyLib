444d66323bbd37d0af36e1ed7c069e9c90e9b699
masatake1115 <masatake1115@hotmail.co.jp>
2022-06-30 11:48:14 +0900

    TODO: Refactor CanvasFrame

--- dev/PyQt/testGraphicsItemLayer/testGraphicsItemLayer/testOverlayViewOnWidget.py
+++ dev/PyQt/testGraphicsItemLayer/testGraphicsItemLayer/testOverlayViewOnWidget.py
@@ -15,70 +15,6 @@ import externalappwidget
 
 
 
-#class ExternalAppWidget( QWidget ):
-
-#    def __init__( self, parent=None ):
-#        super( ExternalAppWidget, self ).__init__( parent=parent )
-
-#        self.setLayout( QVBoxLayout() ) 
-#        self.layout().setContentsMargins( 0, 0, 0, 0 )
-
-#        self.__m_AppWindow = None
-#        self.__m_WindowWidget = None
-
-
-#    #def __del__( self ):
-#    #    #self.__p.terminate()
-#    #    if( self.__m_AppWindow ):
-#    #        self.__m_AppWindow.close()
-
-
-#    def BindHwnd( self, window_handle, processhandle ):
-        
-#        self.__m_AppWindow =  QWindow.fromWinId( window_handle )
-#        self.__m_WindowWidget = QWidget.createWindowContainer( self.__m_AppWindow, self )
-
-#        self.layout().addWidget( self.__m_WindowWidget )
-
-
-#    def UnbindHWnd( self ):
-#        try:
-#            # 外部アプリ埋め込んだwindowと、はめ込んでる__m_WindowWidgetを、下記の順番でSetParentする必要がある
-#            if( self.__m_WindowWidget ):
-#                self.__m_WindowWidget.setParent( None )
-#                self.__m_AppWindow.setParent( None )
-
-#                self.__m_WindowWidget = None
-#                self.__m_AppWindow = None
-
-#        except Exception as e:
-#            import traceback
-#            traceback.print_exc()
-
-
-#    def mousePressEvent( self, event ):
-#        print( "ExternalAppWidget::mousePressEvent" )
-#        return super(ExternalAppWidget, self).mousePressEvent(event)
-
-
-#    def mouseReleaseEvent( self, event ):
-#        print( "ExternalAppWidget::mouseReleaseEvent" )
-#        return super(ExternalAppWidget, self).mouseReleaseEvent(event)
-
-
-#    def keyPressEvent( self, event ):
-#        print( "ExternalAppWidget::keyPressEvent" )
-#        return super(ExternalAppWidget, self).keyPressEvent( event )
-
-
-#    def closeEvent( self, event ):
-#        print( "MyWidget::closeEvent" )
-#        self.UnbindHWnd()
-#        return super(ExternalAppWidget, self).closeEvent( event )
-
-
-
-
 class CanvasFrame( QFrame ):
 
     def __init__(self, parent=None):
@@ -86,12 +22,8 @@ class CanvasFrame( QFrame ):
         self.setLayout( QVBoxLayout() )
         self.layout().setContentsMargins( 0, 0, 0, 0 )
 
-        self.nonlayoutwidgets = []
-
-
-#TODO: Add self.__m_BackgroundWidget
-#TODO: Add self.__m_OverlayWidget
-#TODO
+        self.__m_refBackground = None   # Background QWidget 
+        self.__m_refOverlay = None      # Overlay QGraphicsView
 
 
     def resizeEvent( self, event ):
@@ -99,44 +31,70 @@ class CanvasFrame( QFrame ):
         r = self.rect()#self.layout().contentsRect()#
         gpos = self.mapToGlobal(r.topLeft())
 
-        for w in self.nonlayoutwidgets:
-            w.setGeometry( gpos.x(), gpos.y(), r.width(), r.height() )#w.resize( r.width(), r.height() )# event.size() )#
-            print( "resizing: ", w.geometry() )
+        if( self.__m_refOverlay ):
+            self.__m_refOverlay.setGeometry( gpos.x(), gpos.y(), r.width(), r.height() )#w.resize( r.width(), r.height() )# event.size() )#
+        #for w in self.nonlayoutwidgets:
+        #    w.setGeometry( gpos.x(), gpos.y(), r.width(), r.height() )#w.resize( r.width(), r.height() )# event.size() )#
+        #    print( "resizing: ", w.geometry() )
         event.accept()
 
 
-    def AddChildWidget( self, w ):
-        w.setParent( self )
-        self.nonlayoutwidgets.append(w)
+    def BindBackground( self, widget: 'QWidget' ):
+
+        self.UnbindBackground()
+
+        self.layout().addWidget( widget )
+        self.__m_refBackground = widget
 
 
-    def RemoveChildWidget( self, w ):
-        self.nonlayoutwidgets.remove(w)
-        w.setParent( None )
+    def UnbindBackground( self ):
+        if( self.__m_refBackground ):
+            self.__m_refBackground.setParent( None )
+            self.__m_refBackground = None
 
 
-    def mousePressEvent( self, event ):
-        print( "CanvasFrame::mousePressEvent" )
-        return super(CanvasFrame, self).mousePressEvent( event )
+    def BindOverlay( self, view: 'QGraphicsView' ):
+
+        self.UnbindOverlay()
+
+        self.__m_refOverlay = view
+        self.__m_refOverlay.setParent( self )        
+        self.__m_refOverlay.setAttribute( Qt.WA_NoSystemBackground )# バックグラウンドなし
+        self.__m_refOverlay.setAttribute( Qt.WA_TranslucentBackground )# ビューの背景を完全透明化する
+        self.__m_refOverlay.setWindowFlags( Qt.Tool | Qt.FramelessWindowHint | Qt.WindowDoesNotAcceptFocus )# 枠なしカバーウィンドウにする. Qt.WindowStaysOnTopHint: 全画面上で常に最前面にView表示させるフラグ.これは外す
+        # プラットフォーム依存のQt.CoverWindowの代わりにQt.Toolを使っている
 
 
+    def UnbindOverlay( self ):
+        if( self.__m_refOverlay ):
+            self.__m_refOverlay.setParent( None )
+            self.__m_refOverlay = None
 
-    def FindTopLevelWidget( self ):
+
+    def SetupOverlayMoveEvent( self ):
+        # Find top level widget
         top_widget = self
         while( top_widget.parentWidget() ):
             top_widget = top_widget.parentWidget()
-
-        # TODO: Install EventFilter
+        # install event filter for moveEvent
         if( top_widget ):
             print( "installing event filter...", top_widget )
             top_widget.installEventFilter( self )# MyEventFilter(top_widget, self) )
 
 
 
+    def mousePressEvent( self, event ):
+        print( "CanvasFrame::mousePressEvent" )
+        return super(CanvasFrame, self).mousePressEvent( event )
+
+    
     def eventFilter( self, obj: 'QObject', event: 'QEvent') -> bool:
-        gpos = self.mapToGlobal( self.rect().topLeft() )
-        for w in self.nonlayoutwidgets:
-            w.move( gpos )
+
+        if( event.type() == QEvent.Move and self.__m_refOverlay ):
+            gpos = self.mapToGlobal( self.rect().topLeft() )
+            self.__m_refOverlay.move( gpos )
+            #for w in self.nonlayoutwidgets:
+            #    w.move( gpos )
 
         return super(CanvasFrame, self).eventFilter(obj, event)
 
@@ -204,14 +162,10 @@ class windowOverlay(QWidget):
 
         #================ compose canvasFrame ==================#
         self.canvasFrame = CanvasFrame()
-        
-        self.canvasFrame.layout().addWidget( self.backgroundWidget )# 埋め込みアプリ背景ウィンドウはQLayoutに登録する
+        self.canvasFrame.BindBackground( self.backgroundWidget )
+        self.canvasFrame.BindOverlay( self.view )#
 
-        self.canvasFrame.AddChildWidget( self.view )# QLayoutではなく直接QWidgetの子供にする & viewのアトリビュートを変更する
-        self.view.setAttribute( Qt.WA_NoSystemBackground )# バックグラウンドなし
-        self.view.setAttribute( Qt.WA_TranslucentBackground )# ビューの背景を完全透明化する
-        self.view.setWindowFlags( Qt.Tool | Qt.FramelessWindowHint | Qt.WindowDoesNotAcceptFocus )# 枠なしカバーウィンドウにする. Qt.WindowStaysOnTopHint: 全画面上で常に最前面にView表示させるフラグ.これは外す
-        # プラットフォーム依存のQt.CoverWindowの代わりにQt.Toolを使っている
+
 
         self.button = QPushButton("Toggle Overlay")
         self.button.setFixedHeight(50)
@@ -225,7 +179,7 @@ class windowOverlay(QWidget):
         self.verticalLayout.addWidget( self.button )
         self.verticalLayout.addWidget( self.button2 )
 
-        #self.layout().setContentsMargins( 0, 0, 0, 0 )
+        self.layout().setContentsMargins( 0, 0, 0, 0 )
 
         
 
@@ -283,7 +237,7 @@ class windowOverlay(QWidget):
 
 
     def AAAA( self ):
-        self.canvasFrame.FindTopLevelWidget()
+        self.canvasFrame.SetupOverlayMoveEvent()
         main.view.show()
 
 
